b12e0019cd08efaa6994a1b0c7e8f703e0556b52
unknown <david.silva425@etec.sp.gov.br>
2022-06-10 00:34:32 -0300

    aplicação que mostra a geocalização do usuario em Cordova

---
+++ Geocalização/www/js/index.js
@@ -0,0 +1,51 @@
+
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onGeolocationSucess(position){
+    alert('Latitude: '          + position.coords.latitude          + '\n' +
+    'Longitude: '         + position.coords.longitude         + '\n' +
+    'Altitude: '          + position.coords.altitude          + '\n' +
+    'Accuracy: '          + position.coords.accuracy          + '\n' +
+    'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+    'Heading: '           + position.coords.heading           + '\n' +
+    'Speed: '             + position.coords.speed             + '\n' +
+    'Timestamp: '         + position.timestamp                + '\n');
+
+
+    document.getElementById("latitude").value=position.coords.latitude;
+    document.getElementById("longitude").value=position.coords.longitude;
+    document.getElementById("altitude").value=position.coords.altitude;
+    document.getElementById("accuracy").value=position.coords.accuracy;
+    document.getElementById("altitudeAccuracy").value=position.coords.altitudeAccuracy;
+    document.getElementById("heading").value=position.coords.heading;
+    document.getElementById("speed").value=position.coords.speed;
+
+
+
+}
+function stopObservePosition(){
+    var watchID = navigator.geolocation.watchPosition(window.watchID);
+}
+
+function onGeolocationError(error){
+    alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+
+}
+
+optionsLocation = {
+    maximumAge: 3000,
+    timeout: 5000,
+    enableHighAccuracy: true
+}
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+
+    alert("Geocalização obtida... ");
+    alert(navigator.geolocation);
+
+    window.watchID = navigator.geolocation.watchPosition(onGeolocationSucess,onGeolocationError,optionsLocation);
+    
+}
